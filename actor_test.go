4f3372ddd0b2ec70e27d8659b7db2ac3f8765b02
Aaron Price <aaronjprice@protonmail.com>
2019-07-13 09:36:19 +0100

    Added test that diff actors can act concurrently

--- actor_test.go
+++ actor_test.go
@@ -22,3 +22,56 @@ func TestExecution(t *testing.T) {
 		t.Error("Actor failed to execute function.")
 	}
 }
+
+func TestDiffHashIDConcurrentExecution(t *testing.T) {
+	var userIDA int64 = 1
+	var userIDB int64 = 2
+
+	concurrentTestFunGen := func(stateChan chan<- string, signalChan <-chan interface{}) func() interface{} {
+		return func() interface{} {
+			stateChan <- "executing"
+			<-signalChan
+			stateChan <- "done"
+			return nil
+		}
+	}
+
+	stateChanA := make(chan string)
+	signalChanA := make(chan interface{})
+	testFunA := concurrentTestFunGen(stateChanA, signalChanA)
+
+	stateChanB := make(chan string)
+	signalChanB := make(chan interface{})
+	testFunB := concurrentTestFunGen(stateChanB, signalChanB)
+
+	userActor := New()
+	userActor.Execute(userIDA, testFunA)
+	userActor.Execute(userIDB, testFunB)
+
+	select {
+	case <-stateChanA:
+	case <-time.After(1 * time.Second):
+		t.Error("Actor failed to execute function A.")
+	}
+
+	select {
+	case <-stateChanB:
+	case <-time.After(1 * time.Second):
+		t.Error("Actor failed to execute function B.")
+	}
+
+	close(signalChanA)
+	close(signalChanB)
+
+	select {
+	case <-stateChanA:
+	case <-time.After(1 * time.Second):
+		t.Error("Actor failed to execute function A.")
+	}
+
+	select {
+	case <-stateChanB:
+	case <-time.After(1 * time.Second):
+		t.Error("Actor failed to execute function B.")
+	}
+}
